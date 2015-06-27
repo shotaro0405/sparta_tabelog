c1dcbf9ec0758910982c4f970fcac56d91af26c0
瀬崎正太郎 <09hn054s@gmail.com>
2015-06-27 18:31:22 +0900

    deviseにusername, image_url追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,32 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+  end
+
+  def create
+     @review = Review.new(
+      restaurant_id: params[:restaurant_id],
+      user_id: current_user.id,
+      score: review_params["score"],
+      title: review_params["title"],
+      body: review_params["body"]
+    )
+    if @review.save
+      redirect_to restaurant_url(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(
+      :score,
+      :title,
+      :body
+      )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627063109) do
+ActiveRecord::Schema.define(version: 20150627085929) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20150627063109) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
